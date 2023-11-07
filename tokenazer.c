8b27a571cc58df616781289d57dbc2b032f9298a
MohamedAmhal <mohammedamhal349@gmail.com>
2023-11-07 09:32:56 -0800

    adding the function tokenizer

---
+++ tokenazer.c
@@ -0,0 +1,55 @@
+#include "shell.h"
+
+/**
+ * tokenizer - this is a function that return the commands
+ * @ligne: the command
+ * Return: two the array
+ */
+
+char **tokenizer(char *ligne)
+{
+	char *tmp = NULL;
+	char **command = NULL;
+	int c = 0;
+	int i = 0;
+	char *token = NULL;
+
+	if (!ligne)
+		return (NULL);
+	tmp = _strdup(ligne);
+	token = strtok(tmp, DEL);
+	if (token == NULL)
+	{
+		free(tmp);
+		tmp = NULL;
+		free(ligne);
+		ligne = NULL;
+		return (NULL);
+	while (token)
+	{
+		c++;
+		token = strdok(NULL, DEL);
+	}
+	free(tmp);
+	tmp = NULL;
+
+	command = malloc(sizeof(char *) * (c + 1));
+	if(!command)
+	{
+		free(ligne);
+		ligne = NULL;
+		return (NULL);
+
+	token = strtok(ligne, DEL);
+
+	while(token)
+	{
+		command[i] = _strdup(token);
+		token = strtok(NULL, DEL);
+		i++;
+	}
+	free(ligne);
+	ligne = NULL;
+	command[i] = NULL;
+	return (command);
+}
