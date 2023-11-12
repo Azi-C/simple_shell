ee844dc9593b8698eeff9730c5b7bd968f42f087
Azi-C <aaziza.chbihi@gmail.com>
2023-11-12 11:47:06 -0800

    modifications

--- chain.c
+++
@@ -1,6 +0,0 @@
-#include "shell.h"
-
-/**
- * chain - tests if is a chain delimeter
- * @prm: the parameter struct
- * @arr: the char 
--- fct_built.c
+++ fct_built.c
@@ -42,8 +42,6 @@ void hd_buil(char **commande, char **argv, int *status, int idx)
 		print_unsetenv(commande);
 	else if (_strcmp(commande[0], "cd") == 0)
 		command_cd(commande);
-	else if (_strcmp(commande[0], "alias") == 0)
-		alias_fct(commande);
 }
 
 /**
